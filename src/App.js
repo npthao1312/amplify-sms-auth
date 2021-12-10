1b0d56feff84f887c18e198f74cf878fa4a0e92c
ThaoNP <npthao1312@gmail.com>
2021-12-10 14:53:26 +0700

    Basic Auth - But no phone verify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  ConfirmSignIn,
+  ConfirmSignUp,
+  ForgotPassword,
+  RequireNewPassword,
+  SignIn,
+  SignUp,
+  VerifyContact,
+  withAuthenticator,
+} from "aws-amplify-react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
-export default App;
+const signUpConfig = {
+  header: "Create an Account",
+  hideAllDefaults: true,
+  defaultCountryCode: "1",
+  signUpFields: [
+    {
+      label: "Phone number",
+      key: "username",
+      required: true,
+      displayOrder: 1,
+      type: "string",
+    },
+    {
+      label: "Password",
+      key: "password",
+      required: true,
+      displayOrder: 2,
+      type: "password",
+    },
+  ],
+};
+
+export default withAuthenticator(App, {
+  includeGreetings: true,
+  signUpConfig,
+});
+
+// export default withAuthenticator(App, false, [
+//   <ConfirmSignIn />,
+//   <VerifyContact />,
+//   <SignUp />,
+//   <ConfirmSignUp />,
+//   <ForgotPassword />,
+//   <RequireNewPassword />,
+// ]);
